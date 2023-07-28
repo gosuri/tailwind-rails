1bbc8cffecb0e558c12c38edf78f78b3747468e4
Greg Osuri <me@gregosuri.com>
2023-07-27 19:14:28 -0500

    ...

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+const akashAI = require('akashai');
+
+akashAI.models.get('stable-diffusion').then(model => {
+    model.predict("origami style, an astronaut riding a horse, studio lighting, white background, pastel colors")
+});
